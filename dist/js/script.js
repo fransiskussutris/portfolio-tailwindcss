bbac966e78192a40de96da7af83c283c77ca5dc6
Pranciskus Sutris <fransiskussutris@gmail.com>
2023-09-23 11:01:35 +0700

    portfolio tailwindcss

---
+++ dist/js/script.js
@@ -0,0 +1,18 @@
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.scrollY > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+
+const humberger = document.querySelector("#humberger");
+const navMenu = document.querySelector("#nav-menu");
+
+humberger.addEventListener("click", function () {
+  humberger.classList.toggle("humberger-active");
+  navMenu.classList.toggle("hidden");
+});
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: { primary: "#900DDA", dark: "#0f172a", secondary: "#6B7280" },
+      screens: { "2xl": "1320px" },
+    },
+  },
+  plugins: [],
+};
